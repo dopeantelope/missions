f042b28c26342f4bdd9562a4d2fb211baa3de50b
Georgie Conway <dopestantelope@gmail.com>
2022-05-08 19:55:57 +0100

    added some mission data

--- client/your-missions.js
+++ client/your-missions.js
@@ -1,3 +1,6 @@
+
+
+
 document.querySelector('*').addEventListener('click', doubleClick)
 
 let isEncoded = false
@@ -7,6 +10,7 @@ function doubleClick() {
     for (const elem of document.getElementsByClassName('mission-text')) {
       elem.style.textTransform = 'none'
       elem.innerText = btoa(elem.innerText)
+      
     }
     isEncoded = true
   } else {
@@ -17,38 +21,4 @@ function doubleClick() {
     isEncoded = false
   }
   console.log('hello')
-}
-
-touchableElement.addEventListener('touchstart', function (event) {
-  touchstartX = event.changedTouches[0].screenX;
-  touchstartY = event.changedTouches[0].screenY;
-}, false);
-
-touchableElement.addEventListener('touchend', function (event) {
-  touchendX = event.changedTouches[0].screenX;
-  touchendY = event.changedTouches[0].screenY;
-  handleGesture();
-}, false);
-
-
-function handleGesture() {
-  if (touchendX < touchstartX) {
-      document.querySelector('body').style.background = black;
-  }
-
-  if (touchendX > touchstartX) {
-      console.log('Swiped Right');
-  }
-
-  if (touchendY < touchstartY) {
-      console.log('Swiped Up');
-  }
-
-  if (touchendY > touchstartY) {
-      console.log('Swiped Down');
-  }
-
-  if (touchendY === touchstartY) {
-      console.log('Tap');
-  }
 }
